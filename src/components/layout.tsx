76c70a5eb837837946d4824006806cac78cf0ea5
Paul Pan <panjunhong05@gmail.com>
2019-10-29 03:35:48 -0700

    Added sass plugin for Gatsby

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 interface LayoutProps {
   readonly children: React.ReactNode | readonly React.ReactNode[]
